fe8415772818a9aff96d736a11ca7d95cf8e4bcf
Anna Becker and Brad London <anna.r.becker@gmail.com>
2016-03-22 11:58:50 -0700

    Add more functionality.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,28 +1,31 @@
-import { Component } from 'angular2/core';
+import { Component, EventEmitter } from 'angular2/core';
+import { TaskListComponent } from './task-list.component';
+import { Task } from './task.model';
 
 @Component({
   selector: 'my-app',
+  directives: [TaskListComponent],
   template: `
-  <div class="container">
-    <h1>To-DO List</h1>
-    <h3 *ngFor="#task of tasks">{{ task.description }}</h3>
-  </div>
-    `
+    <div class="container">
+      <h1>To-Do List</h1>
+      <task-list
+        [taskList]="tasks"
+        (onTaskSelect)="taskWasSelected($event)">
+      </task-list>
+    <div>
+  `
 })
-
 export class AppComponent {
-  public task: Task[];
+  public tasks: Task[];
   constructor(){
     this.tasks = [
-      new Task("Create To-Do List app." , 0),
-      new Task("Learn Kung Fu." , 1),
+      new Task("Create To-Do List app.", 0),
+      new Task("Learn Kung Fu.", 1),
       new Task("Rewatch all the Lord of the Rings movies.", 2),
       new Task("Do the laundry.", 3)
     ];
   }
-}
-
-export class Task {
-  public done: boolean = false;
-  constructor(public description: string, public id: number) {}
+  taskWasSelected(clickedTask: Task): void {
+    console.log('parent', clickedTask);
+  }
 }
